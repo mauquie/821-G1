82badd4909cfc1e398beaaec063052474928e713
Guiraud Yoan <37932069+yoan82@users.noreply.github.com>
2020-04-15 15:49:43 +0200

    Changement du nom de la BD et contrainte apportée dans le mot de passe 8th Push - BD : siteweb_grp1

--- SiteWeb-G1-Symfony/src/Entity/User.php
+++ SiteWeb-G1-Symfony/src/Entity/User.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
@@ -28,6 +29,8 @@ class User
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Length(min=”8”, minMessage=”Votre mot de passe est trop court”) 
+     * @Assert\EqualTo(propertyPath=”confirm_password”, message=”Vous n’avez pas tapé le même mot de passe”) 
      */
     private $password;
     
