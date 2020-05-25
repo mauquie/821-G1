5306ff135194ecf3630583162d43dd3d4663f4ec
Guiraud Yoan <37932069+yoan82@users.noreply.github.com>
2020-05-25 11:03:41 +0200

    Save Page emprunts 25/05/2020

--- SiteWeb-G1-Symfony/src/Controller/BorrowController.php
+++ SiteWeb-G1-Symfony/src/Controller/BorrowController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Entity\Borrow;
+use App\Repository\UserRepository;
 use App\Repository\BorrowRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -17,12 +19,22 @@ class BorrowController extends AbstractController
      * @Route("/emprunt", name="borrow")
      * @Security("is_granted('ROLE_USER')")
      */
-    public function index(BorrowRepository $repository)
+    public function index(BorrowRepository $repositoryBorrow, UserRepository $repositoryUser, Borrow $borrow)
     {
+        $user->getEmail();
+        $email = $repository->findBy([ 'user' => $user ]);
+        
+        $equipment = $borrow->getEquipment();
+        $borrowStart = $borrow->getBorrowStart();
+        $borrowEnd = $borrow->getBorrowEnd();
+        $quantity = $borrow->getQuantity();
+        
+        $borrows = $repository->find($email, $equipment, $borrowStart, $borrowEnd, $quantity);
         
         return $this->render('borrow/borrows.html.twig', [
             'current_menu' => 'active_borrow',
-            'borrows' => $repository->findAll()
+            'borrows' => $borrows,
+            'email' => $user
         ]);
     }
     
