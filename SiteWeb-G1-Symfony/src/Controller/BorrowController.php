a83876cf078710eda57e9f557f59ddd9bdceaeaf
LaymajouxClement <laymajouxclement@gmail.com>
2020-08-12 23:03:32 +0200

    borrow user v 1.0

--- SiteWeb-G1-Symfony/src/Controller/BorrowController.php
+++ SiteWeb-G1-Symfony/src/Controller/BorrowController.php
@@ -3,13 +3,17 @@
 namespace App\Controller;
 
 use App\Entity\Equipment;
+use App\Entity\User;
+use App\Entity\Borrow;
+use App\Form\BorrowType;
 use App\Repository\EquipmentRepository;
 //use App\Repository\BorrowRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
-
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
 
 class BorrowController extends AbstractController
 {
@@ -18,18 +22,36 @@ class BorrowController extends AbstractController
      * @Route("/emprunter/{slug}", name="borrow")
      * @Security("is_granted('ROLE_USER')")
      */
-    public function index(EquipmentRepository $equipments, string $slug)
+    public function index(EquipmentRepository $equipments, string $slug, Request $request, EntityManagerInterface $manager)
     {
+        
+        $newBorrow = new Borrow();
+        
         // On récupère l'utilisateur connecté
-        $user = $this->getUser();   
+        $user = $this->getUser();  
         
         // On récupère l'équipement qui appartient au slug de la page emprunte
-        $equipment = $equipments->findOneBy([ 'slug' => $slug ]);  
+        $equipment = $equipments->findOneBy([ 'slug' => $slug ]);
         dump($equipment);
+        
+        
+        $form = $this->createForm(BorrowType::class,$newBorrow,['quantity'=> $equipment->getStock()]);              
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+            $newBorrow->setUser($user);
+            $newBorrow->setEquipment($equipment);
+            
+            $manager->persist($newBorrow);
+            $manager->flush();
+            
+            return $this->redirectToRoute('my_borrowings');
+        }
         
         return $this->render('borrow/index.html.twig', [
             'current_menu' => 'active_equipment',
-            'equipment' => $equipment
+            'equipment' => $equipment,
+            'form' =>$form->createView()
         ]);
     }
 }
---
+++ SiteWeb-G1-Symfony/src/Form/BorrowType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Borrow;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+class BorrowType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $quantity = $options['quantity'];
+        $choices=[];
+        while ($quantity > 0){
+            $choices[strval($quantity)] = $quantity;
+            $quantity = $quantity -1;
+        }
+        $builder
+            ->add('borrow_start')
+            ->add('borrow_end')
+            ->add('quantity',ChoiceType::class,[
+                'choices' => $choices
+            ])            
+        ;                        
+    }
+    
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([ 'data_class' => Borrow::class ]);
+        $resolver->setRequired(['quantity']);
+    }
+}
