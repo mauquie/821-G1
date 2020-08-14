29aed180a9fcfaccdeec8ee664d65b72d9f00bdf
LaymajouxClement <laymajouxclement@gmail.com>
2020-08-14 11:17:32 +0200

    change borrow's form to the current date

--- SiteWeb-G1-Symfony/src/Form/BorrowType.php
+++ SiteWeb-G1-Symfony/src/Form/BorrowType.php
@@ -7,19 +7,25 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use \DateTime;
 
 class BorrowType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $quantity = $options['quantity'];
+       
         $choices=[];
         while ($quantity > 0){
             $choices[strval($quantity)] = $quantity;
             $quantity = $quantity -1;
         }
+        
+        $date=new \DateTime("now");
+        $date = date_format($date, "d-m-Y H:00");
+        
         $builder
-            ->add('borrow_end')
+            ->add('borrow_end',null,array('data'=>DateTime::createFromFormat("d-m-Y H:00",$date)))
             ->add('quantity',ChoiceType::class,[
                 'choices' => $choices,
                 'data' => 1
